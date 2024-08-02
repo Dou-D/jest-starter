357c8569c72c551dc0c527b562c208ed4ea245ec
dou <15309487104@163.com>
2024-08-02 22:15:14 +0800

    jest-location-mock

--- jest.config.js
+++ jest.config.js
@@ -146,7 +146,7 @@ const config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: 'jest-environment-jsdom-global',
+  testEnvironment: 'jsdom',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
--- tests/utils/getSearchObj.test.ts
+++ tests/utils/getSearchObj.test.ts
@@ -2,9 +2,7 @@ import { getSearchObj } from "utils/getSearchObj";
 
 describe("getSearchObj", () => {
   it("can get current route params object", () => {
-    globalThis.jsdom.reconfigure({
-      url: "https://www.baidu.com?a=1&b=2",
-    });
+    window.location.assign("https://www.baidu.com?a=1&b=2")
 
     expect(window.location.search).toEqual("?a=1&b=2");
     expect(getSearchObj()).toEqual({
@@ -13,9 +11,7 @@ describe("getSearchObj", () => {
     });
   });
   it("can return null when not pass a param", () => {
-    globalThis.jsdom.reconfigure({
-      url: "https://www.baidu.com",
-    });
+    window.location.assign("https://www.baidu.com")
     expect(window.location.search).toEqual("");
     expect(getSearchObj()).toEqual({});
   });
